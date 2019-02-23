0cc4d50f09864b915c6c2eabf16d6dbcfd8de201
AareanaReza <areza5@illinois.edu>
2019-02-23 17:34:05 -0600

    Added get_article_text function which takes in any article url and returns its text

--- ScienceDailyWebScraper.py
+++ ScienceDailyWebScraper.py
@@ -7,14 +7,26 @@ import urllib3
 
 http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 
-# Parsing url to HTML tags
-url = 'https://www.sciencedaily.com/releases/2019/02/190221141511.htm'
-response = http.request('GET', url)
-soup = BeautifulSoup(response.data, features="html.parser")
 
-# Takes all the <p> tags in the <div> tagged block with id= 'text'
-div = soup.find('div', {'id': 'text'})  # attrs={'id': 'story_text'}
-print(soup.find('p', {'id': 'first'}).text)
-for p in div.findAll('p'):
-    print(p.text)
-    print("%%%")
+# Takes in the article url and returns only the text within that article
+def get_article_text(article_url):
+    # Parsing url to HTML tags
+    url = article_url
+    response = http.request('GET', url)
+    soup = BeautifulSoup(response.data, features="html.parser")
+    final_text = ""
+
+    title = soup.find('h1', {'id': 'headline'}).text
+    # There is a '$$$' after the title to help separate it from the rest of the article
+    final_text = final_text + title + "$$$"
+    # Takes all the <p> tags in the <div> tagged block with id= 'text'
+    div = soup.find('div', {'id': 'text'})  # attrs={'id': 'story_text'}
+    final_text = final_text + soup.find('p', {'id': 'first'}).text + '%%%'
+    for p in div.findAll('p'):
+        # '%%%' after each line of text to help with summarizing article
+        final_text = final_text + p.text + '%%%'
+    return final_text
+
+
+print(get_article_text('https://www.sciencedaily.com/releases/2019/02/190221141511.htm'))
+
