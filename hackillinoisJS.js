7b0d4835b265b02497919404336e0cfe5056d2f2
mheflin259 <mheflin259@gmail.com>
2019-02-23 22:34:31 -0600

    added JS and HTML changes

--- hackillinoisJS.js
+++ hackillinoisJS.js
@@ -1,4 +1,23 @@
+const axios = require('axios');
+
 function myFunction() {
   var x = document.getElementById("input").value;
   document.getElementById("output").innerHTML = x;
+}
+
+function processUrl(url) {
+  // validate URL
+  axios.get(url)
+    .then(function (response) {
+      // handle success
+      console.log(response);
+      getElementById("output") = 
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
 }
