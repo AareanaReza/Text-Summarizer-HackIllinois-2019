1a9f39f65e750889e85634ad89dde39d37793182
cynthia <cd13@gmail.com>
2019-02-24 03:09:43 -0600

    Done!!!!!!!!!!!!!! without revisions

--- article_summarizer.py
+++ article_summarizer.py
@@ -9,14 +9,14 @@ import numpy as np
 
 max_number_of_results = 10
 title_frequency_bonus = 3
-key_words = ["for", "regarding", "concerning", "regard", "concern", "on", "displays", "predict", "how"]
-
+key_words = ["regarding", "concerning", "regard", "concern", "on", "displays", "predict", "how"]
+original_phrase_list = []
 
 # cleans the string - strips all punctuation, trims leading/trailing whitespace, and converts to lowercase
 def clean_string(str):
     str = str
     return str.translate({ord(c): '' for c in string.punctuation}).lower()
-    #return str.translate(string.maketrans("", ""), string.punctuation).strip().lower()
+    # return str.translate(string.maketrans("", ""), string.punctuation).strip().lower()
 
 
 # returns true if the word is considered a stop word
@@ -32,7 +32,8 @@ def find_possible_topics(article_words, key_words, title_words):
             possible_topics.append(word)
 
     for i in range(len(article_words)):
-        if article_words[i] in key_words and article_words[i + 1] not in possible_topics and not is_stop_word(article_words[i]):
+        if article_words[i] in key_words and article_words[i + 1] not in possible_topics and not is_stop_word(
+                article_words[i]):
             possible_topics.append(article_words[i + 1])
     return possible_topics
 
@@ -46,13 +47,14 @@ def clean_words(article_words):
 
 
 # prints the top 10 results for words to complete the first summary sentence
-def print_results(frequencies):
-    sentence_structure = "This article is about... (top word choices) "
+def print_results(frequencies, article, title):
+    '''sentence_structure = "This article is about... (top word choices) "
     print(sentence_structure)
     for i in range(min(len(frequencies), 10)):
-        print(str(i + 1) + ". " + frequencies[i][0])
+        print(str(i + 1) + ". " + frequencies[i][0])'''
 
     print("This article covers information about " + frequencies[0][0] + " and " + frequencies[1][0] + ".")
+    print("The study is about " + get_frequency(remove_stop_words_within_pos_phrase_array(clean_phrase_list(make_phrase_list(volcano_article))), article, title) + ".")
 
 
 # returns a list of words that have the part of speech (noun, plural noun, etc.) we'd use in our first summary sentence
@@ -106,7 +108,7 @@ def summarize_article(article, title):
                          reverse=True,
                          key=lambda x: x[1])
 
-    print_results(frequencies)
+    print_results(frequencies, article, title)
 
 
 def make_phrase_list(article):
@@ -118,13 +120,15 @@ def make_phrase_list(article):
     return phrase_list
 
 
+
 def clean_phrase_list(phrase_list):
     pos_phrase_list = []
     for i in range(len(phrase_list)):
         for word in key_words:
             phrase_list[i] = clean_string(phrase_list[i])
-            if phrase_list[i].__contains__(" " + word):
+            if phrase_list[i].__contains__(" " + word + " "):
                 pos_phrase_list.append(word + phrase_list[i].split(" " + word)[1])
+    original_phrase_list.extend(pos_phrase_list)
     return pos_phrase_list
 
 
@@ -137,6 +141,27 @@ def remove_stop_words_within_pos_phrase_array(pos_phrase_list):
         words_in_phrase_list[i] = get_valid_summary_words(words_in_phrase_list[i])
     return words_in_phrase_list
 
+
+def get_frequency(words_in_phrase_list, article, title):
+    article_words = article.split()
+    title_words = title.split()
+    frequency_of_phrases = []
+    possible_topics = find_possible_topics(article_words, key_words, title_words)
+    # print(possible_topics_pos_list)
+    # possible_topics_pos_list.extend(make_word_pairs(possible_topics))
+    for x in range(len(words_in_phrase_list)):
+        count = 0
+        for i in range(len(words_in_phrase_list[x])):
+            frequencies = {}
+            # find and save the frequencies of all the words in valid_possible_topics
+            for word in words_in_phrase_list[x][i]:
+                if word not in frequencies:
+                    frequencies[word] = article_words.count(word)
+                    count += article_words.count(word)
+        frequency_of_phrases.append(count)
+    return original_phrase_list[frequency_of_phrases.index(max(frequency_of_phrases))]
+
+
 # sample articles
 
 sustainability_title = "Sustainability Operations"
@@ -148,7 +173,9 @@ sleep_article = "A recent study categorizing people with obstructive sleep apnea
 sci_daily_title = "Fungus from the intestinal mucosa can affect lung health. Our microbiome can impair our immune system through the harmless fungus Candida albicans"
 sci_daily_article = "The composition of the microbiome -- the countless bacteria, fungi and viruses that colonize our body surface, skin, intestines or lungs -- makes a decisive contribution to human health or disease. However, biological mechanisms that cause inflammations in the microbiome are still largely unknown. Together with a group of researchers from the University of Kiel and the University Hospital of Schleswig-Holstein, Professer Dr. Oliver Cornely (head of the Center of Excellence for Invasive Fungal Diseases at Cologne University Hospital) has deciphered a mechanism by which specific intestinal microbiota amplify inflammatory reactions in the lungs. The results of the study, published in Cell, could accelerate the development of new therapies for common diseases. 'The fungus Candida albicans, which colonizes the intestines, skin and mucous membranes, is actually harmless', Cornely said. 'However, our study has shown that Candida albicans affects the balance of our immune system.' Candida albicans stimulates the immune system to produce specific defence cells, so-called Th17 cells. However, some of these Th17 cells then attack other fungi, such as Aspergillus fumigatus. This phenomenon is called cross-reactivity. The research showed that immune-compromised individuals have an increased level of cross-reactive Th17 cells in their lung tissue. This concentration is associated with a deterioration of these patients' health. The protective Th17 reaction in the intestine seems to amplify pathogenic immune processes in the lungs. 'With this observation, we were able to show for the first time how a single member of the microbiome, Candida albicans, influences the specific immune response to a large group of other microbes. Immune cross-reactivity is probably a common mechanism by which the microbiome manipulates the immune system -- with both protective and harmful effects', Cornely remarked. Deciphering such specific effects of individual microbes will in future contribute to the development of targeted therapies."
 
+volcano_title = "Do volcanoes or an asteroid deserve blame for dinosaur extinction?"
+volcano_article = "But it leaves unclear to what degree the two catastrophes contributed to the near-simultaneous mass extinction that killed off the dinosaurs and many other forms of life. The research sheds light on huge lava flows that have erupted periodically over Earth's history, and how they have affected the atmosphere and altered the course of life on the planet. In the study, University of California, Berkeley, scientists report the most precise and accurate dates yet for the intense volcanic eruptions in India that coincided with the worldwide extinction at the end of the Cretaceous Period, the so-called K-Pg boundary. The million-year sequence of eruptions spewed lava flows for distances of at least 500 kilometers across the Indian continent, creating the so-called Deccan Traps flood basalts that in some places are nearly 2 kilometers thick. \"Now that we have dated Deccan Traps lava flows in more and different locations, we see that the transition seems to be the same everywhere. I would say, with pretty high confidence, that the eruptions occurred within 50,000 years, and maybe 30,000 years, of the impact, which means they were synchronous within the margin of error, \"said Paul Renne, a professor-in-residence of earth and planetary science at UC Berkeley, director of the Berkeley Geochronology Center and senior author of the study, which will appear online Feb. 21.\"That is an important validation of the hypothesis that the impact renewed lava flows. The new dates also confirm earlier estimates that the lava flows continued for about a million years, but contain a surprise: three-quarters of the lava erupted after the impact. Previous studies suggested that about 80 percent of the lava erupted before the impact. If most of the Deccan Traps lava had erupted before the impact, then gases emitted during the eruptions could have been the cause of global warming within the last 400,000 years of the Cretaceous Period, during which temperatures increased, on average, about 8 degrees Celsius (14.4 degrees Fahrenheit). During this period of warming, species would have evolved suited to hothouse conditions, only to be confronted by global cooling from the dust or by climate cooling gases caused by either the impact or the volcanos. The cold would have been a shock from which most creatures would never have recovered, disappearing entirely from the fossil record: literally, a mass extinction.But if most of the Deccan Traps lava emerged after the impact, this scenario needs rethinking. \"This changes our perspective on the role of the Deccan Traps in the K-Pg extinction,\" said first author Courtney Sprain, a former UC Berkeley doctoral student who is now a postdoc at the University of Liverpool in the United Kingdom. \"Either the Deccan eruptions did not play a role -- which we think unlikely -- or a lot of climate-modifying gases were erupted during the lowest volume pulse of the eruptions. The hypothesis that climate-altering volcanic gases leak out of underground magma chambers frequently, and not just during eruptions, is supported by evidence from present-day volcanos, such as those of the gas-spewing Mt. Etna in Italy and Popocatepetl in Mexico, the researchers said. Magma stewing below the surface is known to transmit gases to the atmosphere, even without eruptions."
 # summarize_article(sustainability_article, sustainability_title)
-#summarize_article(sleep_article, sleep_title)
-print(summarize_article(sci_daily_article, sci_daily_title))
-print(remove_stop_words_within_pos_phrase_array(clean_phrase_list(make_phrase_list(sci_daily_article))))
+# summarize_article(sleep_article, sleep_title)
+summarize_article(volcano_title, volcano_article)
+#print(get_frequency(remove_stop_words_within_pos_phrase_array(clean_phrase_list(make_phrase_list(sci_daily_article))), sci_daily_article, sci_daily_title))
