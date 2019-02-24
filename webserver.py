3ce6a5cd154556b53210dbc37818014b97abcaaf
rdembi2 <rdembi2@illinois.edu>
2019-02-24 01:05:16 -0600

    No more CORS

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_cors import CORS, cross_origin
 import scrapeweb
 app = Flask(__name__)
@@ -13,10 +13,18 @@ cors = CORS(app, resources={r"/foo": {"origins": "http://localhost:5000"}})
 @cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def hello():
     text = scrapeweb.get_article_text('https://www.sciencedaily.com/releases/2019/02/190221141511.htm')
-    response = flask.jsonify(text)
+    response = jsonify(text)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
 @app.route('/')
 def home():
     return render_template('hackillinoisHTML.html')
+
+@app.route('/js')
+def js():
+    return render_template('hackillinoisJS.js')
+
+@app.route('/css')
+def css():
+    return render_template('hackillinoisCSS.css')
